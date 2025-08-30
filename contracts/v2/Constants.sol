d94e8ca03e6388c373628bfc0186277d8181d727
MONTREAL.AI <info@montreal.ai>
2025-08-30 13:31:17 -0400

    feat: hardcode AGIALPHA token constants

---
+++ contracts/v2/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+library Constants {
+    address constant AGIALPHA = 0xA61a3B3a130a9c20768EEBF97E21515A6046a1fA;
+    uint8 constant AGIALPHA_DECIMALS = 18;
+}
+
