5f12575bbf749d47f01d0289344049fb8f17b801
MONTREAL.AI <info@montreal.ai>
2025-11-28 08:01:26 -0500

    Add d3.exports.js to export D3 library

--- alpha_factory_v1/demos/alpha_agi_insight_v1/insight_browser_v1/d3.exports.js
+++ alpha_factory_v1/demos/alpha_agi_insight_v1/insight_browser_v1/d3.exports.js
@@ -1,12 +1,6 @@
-diff --git a/alpha_factory_v1/demos/alpha_agi_insight_v1/insight_browser_v1/d3.exports.js b/alpha_factory_v1/demos/alpha_agi_insight_v1/insight_browser_v1/d3.exports.js
-new file mode 100644
-index 0000000000000000000000000000000000000000..54a3b307a017bbb1c71ae0abc897a442a7d40efc
---- /dev/null
-+++ b/alpha_factory_v1/demos/alpha_agi_insight_v1/insight_browser_v1/d3.exports.js
-@@ -0,0 +1,6 @@
-+// SPDX-License-Identifier: Apache-2.0
-+const d3 = window.d3;
-+if (!d3) {
-+  throw new Error('d3 global not found');
-+}
-+export default d3;
+// SPDX-License-Identifier: Apache-2.0
+const d3 = window.d3;
+if (!d3) {
+  throw new Error('d3 global not found');
+}
+export default d3;
